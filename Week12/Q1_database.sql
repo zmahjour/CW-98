8b05fab48f9863e8bfade6c808685446174aedf2
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-22 15:37:17 +0330

    Q1: fifth query

--- Week12/Q1_database.sql
+++ Week12/Q1_database.sql
@@ -24,3 +24,12 @@ JOIN film_category ON film.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id
 GROUP BY category.name
 HAVING COUNt(*) BETWEEN 60 AND 68;
+
+
+SELECT title, category.name AS category, language.name AS language
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+JOIN language ON language.language_id = film.language_id;
+
+
