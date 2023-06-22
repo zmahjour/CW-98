bedcfe0975320b6e6f7fac42a0203911384403d8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-22 15:14:51 +0330

    Q1: third query

--- Week12/Q1_database.sql
+++ Week12/Q1_database.sql
@@ -3,8 +3,16 @@ FROM film
 JOIN film_category ON film.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id;
 
+
 SELECT title, category.name, release_year
 FROM film
 JOIN film_category ON film.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id
 WHERE category.name IN ('Action', 'Comdedy', 'Family');
+
+
+SELECT category.name, COUNT(*) AS number_of_its_films
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+GROUP BY category.name;
