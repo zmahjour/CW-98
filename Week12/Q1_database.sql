dddabddaed58c35824755522f8adee8903b29d3f
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-22 15:20:29 +0330

    Q1: fourth query

--- Week12/Q1_database.sql
+++ Week12/Q1_database.sql
@@ -16,3 +16,11 @@ FROM film
 JOIN film_category ON film.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id
 GROUP BY category.name;
+
+
+SELECT category.name, COUNT(*) AS number_of_films
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+GROUP BY category.name
+HAVING COUNt(*) BETWEEN 60 AND 68;
