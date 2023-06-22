86f284b463c67bb3670e82325b02801fc53a19e5
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-22 15:06:02 +0330

    Q1: second query

--- Week12/Q1_database.sql
+++ Week12/Q1_database.sql
@@ -1,4 +1,10 @@
-SELECT title, category, release_year
+SELECT title, category.name, release_year
 FROM film
 JOIN film_category ON film.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id;
+
+SELECT title, category.name, release_year
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name IN ('Action', 'Comdedy', 'Family');
