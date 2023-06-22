05c3d28dcffce471cf56cb2c8a15dc7d7b5af920
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-22 16:58:28 +0330

    Q2: eighth query

--- Week12/Q2_database.sql
+++ Week12/Q2_database.sql
@@ -1,3 +1,10 @@
 SELECT title, length
 FROM film
 WHERE length > (SELECT AVG(length) FROM film);
+
+
+SELECT film.film_id, title, return_date
+FROM film
+JOIN inventory ON inventory.film_id = film.film_id
+JOIN rental ON rental.inventory_id = inventory.inventory_id
+WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30'
