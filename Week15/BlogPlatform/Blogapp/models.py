b8141b6be891127045c3497a35dbdbba38b7a85b
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-13 16:18:06 +0330

    BlogPlatform | creating post model

--- Week15/BlogPlatform/Blogapp/models.py
+++ Week15/BlogPlatform/Blogapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField(auto_now_add=True)
