f5458abf5a466909b4016c220ce85b226e4b0b65
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-13 17:02:53 +0330

    BlogPlatform | writing view_post function

--- Week15/BlogPlatform/Blogapp/views.py
+++ Week15/BlogPlatform/Blogapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def view_post(request):
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "all_post.html", context)
