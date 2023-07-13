dcc62d369f54116d7230a418cab3a1f8f1227110
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-13 16:32:48 +0330

    BlogPlatform | making models

--- Week15/BlogPlatform/Blogapp/models.py
+++ Week15/BlogPlatform/Blogapp/models.py
@@ -6,3 +6,11 @@ class Post(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=100)
     publication_date = models.DateField(auto_now_add=True)
+
+
+class Author(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
