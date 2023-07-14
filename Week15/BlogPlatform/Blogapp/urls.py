04b6f802c140e1c5ef4e6250795753eb04c1f4ff
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-14 11:17:09 +0330

    BlogPlatform | making changes to show view details of each post

--- Week15/BlogPlatform/Blogapp/urls.py
+++ Week15/BlogPlatform/Blogapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("home/", views.home),
     path("posts/", views.view_post),
-    path("authors", views.view_author),
+    path("posts/<int:pk>/", views.post_detail, name="post_detail"),
+    path("authors/", views.view_author),
 ]
