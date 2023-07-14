cd12d598fdcdefb9a501632da2d6fe35541773d0
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-14 14:55:47 +0330

    BlogPlatform | establishing the relationships between models

--- Week15/BlogPlatform/Blogapp/models.py
+++ Week15/BlogPlatform/Blogapp/models.py
@@ -1,14 +1,5 @@
 from django.db import models
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.CharField(max_length=100)
-    publication_date = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return f'title: {self.title} | author: {self.author} | publish date: {self.publication_date.strftime("%Y-%m-%d")}'
+from Category.models import Category
 
 
 class Author(models.Model):
@@ -19,11 +10,22 @@ class Author(models.Model):
         return f"name: {self.name}"
 
 
-# class Comment(models.Model):
-#     post = models.CharField(max_length=100)
-#     author = models.CharField(max_length=100)
-#     content = models.TextField()
-#     date = models.DateField(auto_now_add=True)
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f'author: {self.author} | content: {self.content} | date: {self.date.strftime("%Y-%m-%d")}'
+    def __str__(self):
+        return f'title: {self.title} | author: {self.author} | publish date: {self.publication_date.strftime("%Y-%m-%d")}'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'author: {self.author} | content: {self.content} | date: {self.date.strftime("%Y-%m-%d")}'
