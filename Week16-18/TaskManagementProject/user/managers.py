8a44040c5b9241a67bb0406b295a2799df403fe7
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-06 16:07:50 +0330

    chore: write CustomUserManager

--- Week16-18/TaskManagementProject/user/managers.py
+++ Week16-18/TaskManagementProject/user/managers.py
@@ -1,5 +1,28 @@
+from typing import Any
 from django.contrib.auth.models import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    pass
+    def create_user(self, username, email, first_name, last_name, photo, password):
+        if not username:
+            raise ValueError("User must have username")
+        if not email:
+            raise ValueError("User must have email")
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            photo=photo,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, email, first_name, last_name, photo, password):
+        user = self.create_user(username, email, first_name, last_name, photo, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
