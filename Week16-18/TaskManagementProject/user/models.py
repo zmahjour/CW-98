3ed80d59dbb57699549883361e91bf1935ae3e16
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-06 12:48:22 +0330

    chore: define CustomUser attributes

--- Week16-18/TaskManagementProject/user/models.py
+++ Week16-18/TaskManagementProject/user/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractBaseUser
 
 
 class CustomUser(AbstractBaseUser):
-    pass
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to="user_images/", blank=True, null=True)
