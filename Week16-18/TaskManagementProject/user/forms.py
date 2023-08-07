04536831cbab46256e02403e7de93dff5c7bf8f6
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-07 11:17:46 +0330

    chore: create CustomUserCreationForm

---
+++ Week16-18/TaskManagementProject/user/forms.py
@@ -0,0 +1,26 @@
+from typing import Any
+from django import forms
+from .models import CustomUser
+from django.core.exceptions import ValidationError
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="confirm password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "first_name", "last_name", "photo"]
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password1"] and cd["password2"] and cd["password1"] != cd["password2"]:
+            raise ValidationError("passwords don't match")
+        return cd["password2"]
+
+    def save(self, commit: True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
