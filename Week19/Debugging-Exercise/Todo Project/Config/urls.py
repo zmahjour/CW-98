56f9632563d93939c8d27ed9922ff896b05f7e80
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-08 19:07:22 +0330

    chore: make required changes to load admin page

--- Week19/Debugging-Exercise/Todo Project/Config/urls.py
+++ Week19/Debugging-Exercise/Todo Project/Config/urls.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('Home.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("Home.urls")),
 ]
