6cb0130b03c1875ed0e6ade824e6ffb34c5f6e4b
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-11 15:28:15 +0330

    chore: third exercise

--- Week19/JS_Practice/index.js
+++ Week19/JS_Practice/index.js
@@ -8,19 +8,34 @@
 
 // second_exercise
 
-var age = prompt("Please enter your age:");
+// var age = prompt("Please enter your age:");
 
-age = parseInt(age);
+// age = parseInt(age);
 
-if (age >= 0 && age <= 10) {
-    alert("child");
-}
-else if (age >= 11 && age <= 18) {
-    alert("teenager");
-}
-else if (age >= 19 && age <= 30) {
-    alert("young person");
-}
-else if (age > 30) {
-    alert("adult");
+// if (age >= 0 && age <= 10) {
+//     alert("child");
+// }
+// else if (age >= 11 && age <= 18) {
+//     alert("teenager");
+// }
+// else if (age >= 19 && age <= 30) {
+//     alert("young person");
+// }
+// else if (age > 30) {
+//     alert("adult");
+// }
+
+
+// third_exercise
+
+var username = prompt("Enter your username:")
+    document.cookie = "username=" + username;
+
+var cookies = document.cookie.split(';');
+
+for (var i = 0; i < cookies.length; i++) {
+    var x = cookies[i].split("=");
+    if (x[0] == "username"){
+        alert("Hello" + " " + x[1])
+    }
 }
