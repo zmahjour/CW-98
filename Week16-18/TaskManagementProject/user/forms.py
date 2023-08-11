df366274edc5b575bc521a699844d1fbbc632ea0
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-11 11:24:41 +0330

    fix: change username field to email

--- Week16-18/TaskManagementProject/user/forms.py
+++ Week16-18/TaskManagementProject/user/forms.py
@@ -46,7 +46,7 @@ class CustomUserChangeForm(forms.ModelForm):
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={"class": "form-control"})
     )
